288703790686f936e6d3b56d5ad96ad40331da9a
JADundee <Jadundee@icloud.com>
2023-05-04 13:03:00 -0600

    Error in API call

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -30,7 +30,7 @@ export const getWeatherFromCoords = async (locationObj) => {
 export const getCoordsFromApi = async (entryText, units) => {
     const regex = /^\d+$/g;
     const flag = regex.test(entryText) ? "zip" : "q";
-    const url = `https://api.openweathermap.org/data/3.0/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
     const encodedUrl = encodeURI(url);
     try {
         const dataStream = await fetch(encodedUrl);
