54295b7b708490a0bbee6f39742eb2f198e6a00e
JADundee <Jadundee@icloud.com>
2023-05-06 06:23:53 -0600

    Added 6 day forecast functionality

--- dist/js/domFunctions.js
+++ dist/js/domFunctions.js
@@ -69,7 +69,8 @@ export const updateDisplay = (weatherJson, locationObj) => {
     // current conditions
     const ccArray = createCurrentConditionsDivs(weatherJson, locationObj.getUnit());
     displayCurrentConditions(ccArray);
-    // six day forecast
+
+    displaySixDayForecast(weatherJson);
     setFocusOnSearch();
     fadeDisplay();
 };
@@ -237,3 +238,53 @@ const displayCurrentConditions = (currentConditionsArray) => {
         ccContainer.appendChild(cc);
     });
 };
+
+const displaySixDayForecast = (weatherJson) => {
+    for (let i = 1; i <= 6; i++) {
+        const dfArray = createDailyForecastDivs(weatherJson.daily[i]);
+        displayDailyForecast(dfArray);
+    }
+};
+
+const createDailyForecastDivs = (dayWeather) => {
+    const dayAbbreviationText = getDayAbbreviation(dayWeather.dt);
+    const dayAbbreviation = createElem("p", "dayAbbreviation", dayAbbreviationText);
+    const dayIcon = createDailyForecastIcon(
+        dayWeather.weather[0].icon, 
+        dayWeather.weather[0].description);
+    const dayHigh = createElem(
+        "p", 
+        "dayHigh", 
+        `${Math.round(Number(dayWeather.temp.max))}°`);
+    const dayLow = createElem(
+        "p",
+        "dayLow",
+        `${Math.round(Number(dayWeather.temp.min))}°` );
+    return [dayAbbreviation, dayIcon, dayHigh, dayLow];
+};
+
+const getDayAbbreviation = (data) => {
+    const dateObj = new Date(data * 1000);
+    const utcString = dateObj.toUTCString();
+    return utcString.slice(0, 3).toUpperCase();
+};
+
+const createDailyForecastIcon = (icon, altText) => {
+    const img = document.createElement("img");
+    if (window.innerWidth < 768 || window.innerHeight < 1025) {
+        img.src = `https://openweathermap.org/img/wn/${icon}.png`;
+    } else {
+        img.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    }
+    img.alt = altText;
+    return img;
+};
+
+const displayDailyForecast = (dfArray) => {
+    const dayDiv = createElem("div", "forecastDay");
+    dfArray.forEach(el => {
+        dayDiv.appendChild(el);
+    });
+    const dailyForecastContainer = document.getElementById("dailyForecast__contents");
+    dailyForecastContainer.appendChild(dayDiv);
+}
